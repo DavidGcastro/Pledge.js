a4c601e26938bf19277b0605fed12ab32236bd06
David <davidgcastro93@gmail.com>
2018-04-27 16:19:31 -0400

    finished chapter 2

--- src/pledge.js
+++ src/pledge.js
@@ -8,29 +8,45 @@ class $Promise {
         if (!executor || typeof executor !== 'function')
             throw TypeError('/executor.+function/');
         this._state = 'pending';
+
         this._value;
+        //WHY DOESNT THIS WORK?
         // executor.call(this, this._internalResolve, this._internalReject)
-        executor(this._internalResolve.bind(this), this._internalReject.bind(this))
+        executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+
+        this.then = function (successCb, errorCb) {
+            if (this._state !== 'pending') {
+                successCb(this._value);
+            } else {
+                typeof successCb === 'function' ? successCb : (successCb = null);
+                typeof errorCb === 'function' ? errorCb : (errorCb = null);
+                this._handlerGroups.push({
+                    successCb,
+                    errorCb,
+                });
+            }
+        };
+
+        this._handlerGroups = [];
     }
 
     _internalResolve(data) {
-        if (this._state === "pending") {
+        if (this._state === 'pending') {
             this._state = 'fulfilled';
             this._value = data;
-
         }
+
+        if (this._handlerGroups) this._handlerGroups.forEach((elem) => elem.successCb(this._value));
+
     }
 
     _internalReject(errMessage) {
-        if (this._state === "pending") {
-            this._state = "rejected"
+        if (this._state === 'pending') {
+            this._state = 'rejected';
             this._value = errMessage;
         }
     }
-
-    // We will look at static and subclassed methods shortly
 }
-
 
 /*-------------------------------------------------------
 The spec was designed to work with Test'Em, so we don't
